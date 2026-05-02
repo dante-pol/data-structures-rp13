168a9a49c9d5dcc9bd183cd75fc91ffe2be80496
Dmitry Rak <dim.drvlg@gmail.com>
2026-05-02 12:49:18 +0300

    feat: попытка создать УеУе

---
+++ src/queue.py
@@ -0,0 +1,30 @@
+# --------- Queue (очередь) ---------
+
+# ----- Fields () -----
+# 1. head - ссылка на первый элемент
+# 2. tail - ссылка на последний элемент
+# 3. count - кол-во элементов
+
+# ----- Interface (Операции) -----
+# 1. enqueue(data): - добавить элемент в конец очереди
+# 2. dequeue():Any - извлечь элемент начала очереди
+# 3. peek():Any посмотреть первый элемент в очереди
+# 4. is_empty():bool - проверка на пустоту
+
+
+class Queue:
+
+    def __init__(self):
+        pass
+
+    def enqueue(self):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def is_empty(self):
+        pass

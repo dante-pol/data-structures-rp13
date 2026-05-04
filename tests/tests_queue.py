dbd68edb49ccd16b8fb1780a71013a782421cbc9
Yaroslav Volkov <yvi_7693@icloud.com>
2026-05-04 14:41:56 +0300

    feat: добавлены тесты на Queue

---
+++ tests/tests_queue.py
@@ -0,0 +1,22 @@
+from src.queue import Queue
+
+
+def test_enqueue():
+    queue = Queue()
+    queue.enqueue(1)
+
+    assert queue.peek() == 1
+
+def test_dequeue():
+    queue = Queue()
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.dequeue()
+    assert queue.peek() == 2
+
+def test_is_empty_positive():
+    queue = Queue()
+    assert queue.is_empty() == True
+
+    queue.enqueue(1)
+    assert queue.is_empty() == False

634546347872d51b7a37ec16a0c8c04576075408
Yaroslav Volkov <yvi_7693@icloud.com>
2026-05-06 01:02:43 +0300

    feat: добавлены тесты DoubleLinkedList

--- src/double_linked_list.py
+++ src/double_linked_list.py
@@ -42,6 +42,19 @@ class DoubleLinkedList:
         self.__tail = None
 
 
+    def show(self) -> list[Any]:
+        output = []
+
+        iterator = self.__head
+
+        while iterator is not None:
+            output.append(iterator.data)
+
+            iterator = iterator.next
+
+        return output
+
+
     def add_last(self, data: Any) -> None:
         node = DoubleLinkedList.Node(data=data, next=None)
 
@@ -92,9 +105,11 @@ class DoubleLinkedList:
     # Худшее O(1)
 
 
-    def delete_first(self) -> None:
+    def delete_first(self) -> Node | None:
 
         if self.is_empty(): return None
+
+        buff = self.__head
 
         self.__head = self.__head.next
 
@@ -103,21 +118,23 @@ class DoubleLinkedList:
             self.__tail = None
             self.__count -= 1
 
-            return None
+            return buff
 
         self.__head.prev = None
         self.__count -= 1
 
-        return None
+        return buff
 
     # Лучшее O(1)
     # Среднее O(n)
     # Худшее O(n)
 
 
-    def delete_last(self) -> None:
+    def delete_last(self) -> Node | None:
 
         if self.is_empty(): return None
+
+        buff = self.__tail
 
         self.__tail = self.__tail.prev
 
@@ -126,12 +143,12 @@ class DoubleLinkedList:
             self.__head = None
             self.__count -= 1
 
-            return None
+            return buff
 
         self.__tail.next = None
         self.__count -= 1
 
-        return None
+        return buff
 
     # Лучшее O(1)
     # Среднее O(1)
@@ -147,6 +164,11 @@ class DoubleLinkedList:
 
             return None
 
+        if position == self.__count:
+            self.add_last(data)
+
+            return None
+
         node = DoubleLinkedList.Node(data, next=None)
 
         iterator = self.get(position)
@@ -154,9 +176,10 @@ class DoubleLinkedList:
         node.prev = iterator.prev
         node.next = iterator
 
+        iterator.prev.next = node
         iterator.prev = node
 
-        node.prev.next = node
+        self.__count += 1
 
         return None
 
@@ -170,18 +193,22 @@ class DoubleLinkedList:
         if position > self.__count or position < 0: raise ValueError("list index out of range")
 
         if position > self.__count // 2:
+
             iterator = self.__tail
-            move = iterator.prev
+            iiterator = self.__count
+
+            while iiterator != position:
+                iterator = iterator.prev
+                iiterator -= 1
 
         else:
+
             iterator = self.__head
-            move = iterator.next
+            iiterator = 0
 
-        iiterator = 0
-
-        while iiterator == position:
-            iterator = move
-            iiterator += 1
+            while iiterator != position:
+                iterator = iterator.next
+                iiterator += 1
 
         return iterator
 
@@ -196,10 +223,20 @@ class DoubleLinkedList:
 
         if iterator is None: return None
 
+        if iterator.prev is None:
+            node = self.delete_first()
+            return node
+
+        if iterator.next is None:
+            node = self.delete_last()
+            return node
+
         iterator.prev.next = iterator.next
         iterator.next.prev = iterator.prev
 
-        return None
+        self.__count -= 1
+
+        return iterator
 
     # Лучшее O(1)
     # Среднее O(n)
---
+++ tests/tests_double_linked_list.py
@@ -0,0 +1,229 @@
+import pytest
+
+from src.double_linked_list import DoubleLinkedList
+
+
+@pytest.fixture()
+def create_fuel_double_linked_list():
+    list = DoubleLinkedList()
+
+    list.add_last(1)
+    list.add_last(2)
+    list.add_last(3)
+    list.add_last(4)
+
+    return list
+
+@pytest.fixture()
+def create_empty_double_linked_list():
+    list = DoubleLinkedList()
+
+    return list
+
+@pytest.fixture()
+def create_double_linked_list_one_item():
+    list = DoubleLinkedList()
+
+    list.add_last(1)
+
+    return list
+
+
+def test_add_last_positive(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    list.add_last(5)
+
+    assert list.show() == [1,2,3,4,5]
+
+
+def test_add_last_boundary(create_empty_double_linked_list):
+    list = create_empty_double_linked_list
+
+    list.add_last(1)
+
+    assert list.show() == [1]
+
+
+def test_add_head_positive(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    list.add_head(0)
+
+    assert list.show() == [0,1,2,3,4]
+
+
+def test_add_head_boundary(create_empty_double_linked_list):
+    list = create_empty_double_linked_list
+
+    list.add_head(1)
+
+    assert list.show() == [1]
+
+
+def test_delete_first_positive(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    assert list.delete_first().data == 1
+
+    assert list.show() == [2,3,4]
+
+
+def test_delete_first_boundary_empty(create_empty_double_linked_list):
+    list = create_empty_double_linked_list
+
+    assert list.delete_first() is None
+
+    assert list.show() == []
+
+
+def test_delete_first_boundary_one_item(create_double_linked_list_one_item):
+    list = create_double_linked_list_one_item
+
+    assert list.delete_first().data == 1
+
+    assert list.show() == []
+
+
+def test_delete_last_positive(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    assert list.delete_last().data == 4
+
+    assert list.show() == [1,2,3]
+
+
+def test_delete_last_boundary_empty(create_empty_double_linked_list):
+    list = create_empty_double_linked_list
+
+    assert list.delete_last() is None
+
+    assert list.show() == []
+
+
+def test_delete_last_boundary_one_item(create_double_linked_list_one_item):
+    list = create_double_linked_list_one_item
+
+    assert list.delete_last().data == 1
+
+    assert list.show() == []
+
+
+def test_insert_positive(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    list.insert(2, 10)
+    assert list.show() == [1,2,10,3,4]
+
+
+def test_insert_boundary_in_head(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    list.insert(0, 10)
+
+    assert list.show() == [10, 1,2,3,4]
+
+
+def test_insert_boundary_in_tail(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    list.insert(4, 10)
+
+    assert list.show() == [1,2,3,4,10]
+
+
+def test_insert_boundary(create_double_linked_list_one_item):
+    list = create_double_linked_list_one_item
+
+    list.insert(0, 10)
+
+    assert list.show() == [10, 1]
+
+
+def test_insert_negative(create_empty_double_linked_list):
+    list = create_empty_double_linked_list
+
+    with pytest.raises(ValueError):
+        list.insert(1, 10)
+
+
+def test_get_positive(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    assert list.get(2).data == 3
+
+
+def test_get_negative(create_empty_double_linked_list):
+    list = create_empty_double_linked_list
+
+    with pytest.raises(ValueError):
+        list.get(4)
+
+
+def test_remove_positive(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    assert list.remove(2).data == 2
+
+    assert list.show() == [1,3,4]
+
+
+def test_remove_first_item(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    assert list.remove(1).data == 1
+
+    assert list.show() == [2, 3, 4]
+
+
+def test_remove_last_item(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    assert list.remove(4).data == 4
+
+    assert list.show() == [1,2,3]
+
+
+def test_remove_boundary(create_empty_double_linked_list):
+    list = create_empty_double_linked_list
+
+    assert list.remove(2) is None
+
+
+def test_find_positive(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    assert list.find(2).data == 2
+
+
+def test_find_negative(create_empty_double_linked_list):
+    list = create_empty_double_linked_list
+
+    assert list.find(3) is None
+
+
+def test_count_positive(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    assert list.count() == 4
+
+
+def test_count_boundary(create_empty_double_linked_list):
+    list = create_empty_double_linked_list
+
+    assert list.count() == 0
+
+
+def test_is_empty_positive_fuel(create_fuel_double_linked_list):
+    list = create_fuel_double_linked_list
+
+    assert list.is_empty() == False
+
+
+def test_is_empty_positive_empty(create_empty_double_linked_list):
+    list = create_empty_double_linked_list
+
+    assert list.is_empty() == True
+
+
+

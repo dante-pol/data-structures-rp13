708c94bc26dc16a04de929caee0e89481d53b2ee
Yaroslav Volkov <yvi_7693@icloud.com>
2026-05-11 14:39:36 +0300

    feat: добавлен интерфейс PriorityQueue

---
+++ src/priority_queue.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+
+class PriorityQueue:
+
+    def __init__(self):
+        pass
+
+    def enqueue(self, data: Any, priority: int) -> None:
+        pass
+
+    def dequeue(self) -> Any:
+        pass
+
+    def peek(self) -> Any:
+        pass
+
+    def is_empty(self) -> bool:
+        pass

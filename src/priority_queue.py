9196f3a3cc712b6bdbf520defaa2ed8fad0cf3b7
Yaroslav Volkov <yvi_7693@icloud.com>
2026-05-12 19:03:58 +0300

    feat: добавлены тесты для PriorityQueue

--- src/priority_queue.py
+++ src/priority_queue.py
@@ -4,8 +4,8 @@ from typing import Any
 
 class PriorityQueue:
 
-    def __init__(self, heap: BinaryHeap):
-        self.__heap = heap
+    def __init__(self, ):
+        self.__heap = BinaryHeap()
 
     def enqueue(self, priority: int, data: Any) -> None:
         self.__heap.push(priority, data)
@@ -44,10 +44,12 @@ class BinaryHeap:
         self.__heap = []
 
     def push(self, priority: int, data: Any) -> None:
-        self.__heap.append((priority, data))
+        self.__heap.append([priority, data])
         self.__up()
 
-    def pop(self) -> Any:
+    def pop(self) -> Any | None:
+        if self.is_empty(): return None
+
         data = self.__heap[0][1]
 
         self.__heap[0], self.__heap[-1] = self.__heap[-1], self.__heap[0]
@@ -58,6 +60,8 @@ class BinaryHeap:
         return data
 
     def peek(self) -> Any:
+        if self.is_empty(): return None
+
         return self.__heap[0][1]
 
     def is_empty(self) -> bool:
@@ -69,7 +73,7 @@ class BinaryHeap:
 
         while new_item != 0 and self.__heap[new_item][0] < self.__heap[parent][0]:
 
-            self.__heap[new_item][0], self.__heap[parent][0] = self.__heap[parent][0], self.__heap[new_item][0]
+            self.__heap[new_item], self.__heap[parent] = self.__heap[parent], self.__heap[new_item]
 
             new_item = parent
             parent = (new_item - 1) // 2
@@ -78,8 +82,10 @@ class BinaryHeap:
 
         down_item = 0
 
-        while down_item > len(self.__heap):
+        while down_item < len(self.__heap):
             min_child = 2 * down_item + 1
+
+            if min_child >= len(self.__heap): return None
 
             if min_child + 1 < len(self.__heap) and self.__heap[min_child][0] > self.__heap[min_child + 1][0]:
                 min_child += 1
@@ -87,7 +93,7 @@ class BinaryHeap:
             if self.__heap[down_item][0] < self.__heap[min_child][0]:
                 return None
 
-            self.__heap[down_item][0], self.__heap[min_child][0] = self.__heap[min_child][0], self.__heap[down_item][0]
+            self.__heap[down_item], self.__heap[min_child] = self.__heap[min_child], self.__heap[down_item]
 
             down_item = min_child
 
---
+++ tests/tests_priority_queue.py
@@ -0,0 +1,87 @@
+import pytest
+
+from src.priority_queue import PriorityQueue
+
+
+@pytest.fixture()
+def one_priority():
+    priority_queue = PriorityQueue()
+
+    priority_queue.enqueue(2, "twice1")
+    priority_queue.enqueue(2, "twice2")
+    priority_queue.enqueue(2, "twice3")
+
+    return priority_queue
+
+
+@pytest.fixture()
+def three_priority():
+    priority_queue = PriorityQueue()
+
+    priority_queue.enqueue(1, "first")
+    priority_queue.enqueue(2, "twice")
+    priority_queue.enqueue(3, "three")
+    priority_queue.enqueue(1, "first1")
+    priority_queue.enqueue(2, "twice1")
+    priority_queue.enqueue(3, "three1")
+
+    return priority_queue
+
+
+def test_enqueue_positive(one_priority):
+    queue = one_priority
+
+    queue.enqueue(1, "first1")
+
+    assert queue.peek() == "first1"
+
+
+def test_enqueue_boundary():
+    queue = PriorityQueue()
+
+    queue.enqueue(3, "three")
+
+    assert queue.peek() == "three"
+
+
+def test_dequeue_positive(three_priority):
+
+    queue = three_priority
+
+    assert queue.dequeue() == "first"
+    assert queue.dequeue() == "first1"
+    assert queue.dequeue() == "twice"
+    assert queue.dequeue() == "twice1"
+
+
+def test_dequeue_boundary():
+
+    queue = PriorityQueue()
+
+    assert queue.dequeue() is None
+
+
+def test_peek_positive(one_priority):
+
+    queue = one_priority
+
+    assert queue.peek() == "twice1"
+
+
+def test_peek_boundary():
+    queue = PriorityQueue()
+
+    assert queue.peek() is None
+
+
+def test_is_empty_positive_true():
+    queue = PriorityQueue()
+
+    assert queue.is_empty() == True
+
+
+def test_is_empty_positive_false(one_priority):
+    queue = one_priority
+
+    assert queue.is_empty() == False
+
